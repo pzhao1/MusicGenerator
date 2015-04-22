421a6505335fee985150ca1e4f7bf93aa969a594
Chengying Wang <cwang3@swarthmore.edu>
2015-04-22 15:39:23 -0400

    based on Andrew's code, added code to write out the generated music

--- highestPlausibility.py
+++ highestPlausibility.py
@@ -1,6 +1,7 @@
 import transitionTrial as tt
 import random
 import glob
+import midi
 
 def highestPlausibility(tickM, pitchM, velocityM):
 	"""
@@ -16,7 +17,10 @@ def highestPlausibility(tickM, pitchM, velocityM):
 	currentTick = random.choice(tickM.keys())
 	currentPitch = random.choice(pitchM.keys())
 	currentVelocity = random.choice(velocityM.keys())
+	#currentPitch = random.choice([pitch for pitch in pitchM.iterkeys() if sum(pitchM[pitch].values())!=0])
+	#currentVelocity = random.choice([velocity for velocity in velocityM.iterkeys() if sum(velocityM[velocity].values())!=0])
 
+	print "start:", currentTick, currentPitch, currentVelocity
 	index = 0
 	indexpitch = 0
 	indexvelocity = 0
@@ -50,15 +54,51 @@ def highestPlausibility(tickM, pitchM, velocityM):
 				indexvelocity += 1
 				currentVelocity = item
 				break
+		#print "index", index, ":", tickPitchVelocityList[indexpitch]
 
 	print tickPitchVelocityList
 	return tickPitchVelocityList
 
 def main():
-	files = glob.glob('midis/midiworld/classic/bach*.mid')
-	tickM, pitchM, velocityM = tt.getTransitionMatrix(files)
+	inputFiles = glob.glob('midis/midiworld/classic/bach_acttrag.mid')
+	createNewTransition = True
+	if createNewTransition:
+		tickM, pitchM, velocityM = tt.getTransitionMatrix(inputFiles, 'tickM.txt', 'picthM.txt', 'velocityM.txt')
+	else:
+		pass
+		# Somehow read in the transition matrices
+		# Tentative file names are above, but we can change the file format
+		
 
 	tickPitchVelocityList = highestPlausibility(tickM, pitchM, velocityM)
+	outFileName = "midis/new.mid"
+	# Instantiate a MIDI Pattern (contains a list of tracks)
+	pattern = midi.Pattern()
+	# Instantiate a MIDI Track (contains a list of MIDI events)
+	track = midi.Track()
+	# Append the track to the pattern
+	pattern.append(track)
+	
+	prevPitch = None
+	for tick, pitch, velocity in tickPitchVelocityList:
+		# Append the new note
+		track.append(midi.NoteOnEvent(tick=tick, velocity=velocity, pitch=pitch))
+		# Stop the previous note to avoid unpleasant mixing
+		if prevPitch != None and prevPitch != pitch:
+			track.append(midi.NoteOffEvent(tick=tick, pitch=pitch))
+		prevPitch = pitch
+
+	track.append(midi.NoteOffEvent(tick=300, pitch=prevPitch))
+
+	# Add the end of track event, append it to the track
+	eot = midi.EndOfTrackEvent(tick=0)
+	track.append(eot)
+
+	# Save the pattern to disk
+	midi.write_midifile(outFileName, pattern)
+
+	print "\nMusic written to " + outFileName + "\n"
+
 
 if __name__ == '__main__':
 	main()
