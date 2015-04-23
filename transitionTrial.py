0f97c49948ab8b524ee2adbbc44a60b10da331bd
pzhao1 <pzhao1@swarthmore.edu>
2015-04-22 22:56:34 -0400

    Save matrices to file (incomplete)

--- transitionTrial.py
+++ transitionTrial.py
@@ -1,6 +1,18 @@
 import midi
 import glob
 import warnings
+import json
+
+def saveMatrixToFile(matrix, filePath):
+	matrixFile = open(filePath, "w")
+	matrixFile.write(json.dumps(matrix))
+	matrixFile.close()
+	print "Saved matrix to " + filePath
+
+def loadMatrixFromFile(filePath):
+	matrixFile = open(filePath, "r")
+	return json.loads(matrixFile.read())
+
 
 def normalize(matrix):
 	for key1 in matrix:
@@ -81,13 +93,31 @@ def getTransitionMatrix(inFileNames):
 
 
 def main():
+
+	# Code used to generate and save matrices to file
 	files = glob.glob('midis/midiworld/classic/bach*.mid')
-	#print files
 	tickM, pitchM, velocityM = getTransitionMatrix(files)
+
 	# Just checking dimension is correct. 0 is not a key in pitch
 	print "tickM size: ", len(tickM), "*", len(tickM[0])
 	print "pitchM size: ", len(pitchM), "*", len(pitchM[pitchM.keys()[0]])
 	print "velocityM size: ", len(velocityM),"*", len(velocityM[0])
+
+	saveMatrixToFile(tickM, "matrices/bachTickM.dat")
+	saveMatrixToFile(pitchM, "matrices/bachPitchM.dat")
+	saveMatrixToFile(velocityM, "matrices/bachVelocityM.dat")
+	
+
+	# Code used to load matrices from file
+	"""
+	tickM = loadMatrixFromFile("matrices/bachTickM.dat")
+	pitchM = loadMatrixFromFile("matrices/bachPitchM.dat")
+	velocityM = loadMatrixFromFile("matrices/bachVelocityM.dat")
+
+	print "pitchM size: ", len(pitchM), "*", len(pitchM[pitchM.keys()[0]])
+	print "tickM size: ", len(tickM), "*", len(tickM["0"])
+	print "velocityM size: ", len(velocityM),"*", len(velocityM["0"])
+	"""
 
 if __name__ == "__main__":
 	main()
